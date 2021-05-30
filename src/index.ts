43b829d90ab0e1cfa2755b33d260d246cbd8a80e
fz6m <fz6meng@gmail.com>
2021-05-30 18:21:43 +0800

    chore: add custom element class hooks and clear styleText space

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,11 @@ import {
   Placement,
 } from './typings/guide.d';
 
+// import global styles
+import './styles/index.less';
+
 export { IGuide, IStep, SelectorType, ContentType, Placement };
 export default Guide;
+
+// export class hooks
+export * from './constant/className'
--- src/utils/getCusAnchorEl.ts
+++ src/utils/getCusAnchorEl.ts
@@ -1,27 +1,22 @@
 import { ITargetPos } from '../typings/guide';
+import { CUSTOM_ELEMENT_CLASS } from '../constant/className'
 
 /**
  * Get the anchor element where the modal should be attached to.
  * @param selector - The CSS selector of the anchor element, or the anchor element itself.
  */
 export const getCusAnchorEl = (targetPos: ITargetPos): Element => {
-  const {top, left, width, height} = targetPos
 
-  const preCusAnchor = document.querySelector('.custom-anchor')
+  const preCusAnchor = document.querySelector(CUSTOM_ELEMENT_CLASS)
   preCusAnchor && document.body.removeChild(preCusAnchor)
 
-  const cusAnchor:HTMLElement = document.createElement('div')
+  const cusAnchor: HTMLDivElement = document.createElement('div')
 
-  cusAnchor.className = 'custom-anchor'
-  cusAnchor.style.cssText = `
-    position: absolute; 
-    top: ${top}px;
-    left: ${left}px;
-    width: ${width}px;
-    height: ${height}px;
-    pointer-events: none;
-    opacity: 0;
-  `
+  cusAnchor.className = CUSTOM_ELEMENT_CLASS
+  Object.entries(targetPos).forEach(([key, value]: [any, number]) => {
+    cusAnchor.style[key] = value + 'px'
+  })
+
   document.body.appendChild(cusAnchor)
   return cusAnchor
 };
